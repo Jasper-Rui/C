030d67c06e07ddba009686c0ea6b81a0ee7c6049
Haorui Yang <hr972438@dal.ca>
2021-08-01 20:36:53 -0300

    update practice question

--- Aug 1st practice questions/Aug 1st SumofGCDandLCM2.c
+++ Aug 1st practice questions/Aug 1st SumofGCDandLCM2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-
+/*
 //if the input is 100000000000000000 then the old BF is too slow
 long long int gcd (long long int small, long long int large);
 
@@ -12,22 +12,37 @@ long long int gcd (long long int small, long long int large){
     //     8 % 4 = 0
     //then 4 is the gcd
     long long int remaining = large % small;
+    for(remaining; remaining > 0; remaining = small % remaining){
+        if(small % remaining != 0){
+            continue;
+        }
+        break;
+    }
     
     return remaining;
-}
+}*/
 
 
-
-
-
+#include <stdio.h>
 int main(){
 
     long long int input1, input2;
-    scanf("%ld%ld", &input1, &input2);
+    scanf("%lld%lld", &input1, &input2);
     long long int  small = input1 > input2 ? input2 : input1;
     long long int  large = input1 > input2 ? input1 : input2;
+    
+    long long int tmp = 0;
+    long long int n = input1, m = input2;
+    while(tmp = m % n){
+        m = n;
+        n = tmp;
+    }
+    
+    long long int min = input1 * input2 / n;
+    long long int sum = min + n;
 
-    printf("Testing: %d\n", gcd(small,large));
+
+    printf("%lld\n", sum);
 
     //here we assign the samller value to small
     //here we assign the larger value to large
