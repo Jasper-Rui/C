b43edad612803dd21cce0971cffb30e579facebe
Haorui Yang <hr972438@dal.ca>
2021-08-02 12:56:19 -0300

    update new question

--- Aug 2nd practice questions/Traingle.c
+++ Aug 2nd practice questions/Traingle.c
@@ -21,6 +21,7 @@ int main(){
     int c = 0;
     
     while(scanf("%d %d %d", &a, &b, &c) != EOF){
+	//or  ~scanf("%d %d %d", &a, &b, &c) 
         if(a + b > c && a + c > b && b + c > a){
             if(a == b && b == c){
             printf("Equilateral triangle!\n");
