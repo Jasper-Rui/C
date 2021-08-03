1697dfe2749f48c4bc6c7c883a38c8541fed6c8c
Haorui Yang <hr972438@dal.ca>
2021-08-03 12:32:27 -0300

    Aug 3rd

--- Aug 3nd practice questions/ReverseArray.c
+++ Aug 3nd practice questions/ReverseArray.c
@@ -37,3 +37,70 @@ int main(){
 	return 0;
 
 }
+//void Init(int* arr, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		*(arr + i) = i;
+//	}
+//}
+//
+//void Print(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+//
+//void Reverse(int arr[], int sz)
+//{
+//	int l = 0;
+//	int r = sz - 1;
+//
+//	while (l<r)
+//	{
+//		int tmp = arr[l];
+//		arr[l] = arr[r];
+//		arr[r] = tmp;
+//		l++;
+//		r--;
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[10];
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	Init(arr, sz);
+//	Print(arr, sz);
+//	Reverse(arr, sz);
+//	Print(arr, sz);
+//
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5 };
+//	int i = 0;
+//	printf("%d\n", arr[4]);
+//	printf("%d\n", 4[arr]);
+//
+//
+//	//a + b;
+//	//b + a;
+//	//arr[4] -- > *(arr+4) --> *(4+arr) --> 4[arr]
+//	//arr[4];  *(arr+4)
+//
+//	//for (i = 0; i < 5; i++)
+//	//{
+//	//	printf("%p --- %p\n", &arr[i], arr+i);
+//	//}
+//
+//	return 0;
+//}
