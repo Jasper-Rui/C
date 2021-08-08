66615fd9c0c1dc5df9cee7a5623fa08de28fa058
Haorui Yang <hr972438@dal.ca>
2021-08-08 14:38:35 -0300

    update 3120 solution

---
+++ Aug 8th/Assignment2 solution/solution-master/miner/mempool.h
@@ -0,0 +1,14 @@
+//
+// Created by Alex Brodsky on 2021-05-19.
+//
+
+#ifndef MINER_MEMPOOL_H
+#define MINER_MEMPOOL_H
+#include "transactions.h"
+
+extern void mempool_init();
+extern void mempool_add(trx_t *trx);
+extern trx_t * mempool_remove(unsigned int id);
+extern trx_t * mempool_select(int max_size);
+
+#endif //MINER_MEMPOOL_H
