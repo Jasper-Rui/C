2159971d2c70055f30c9c448ac850f9d8c4d44dc
Haorui Yang <hr972438@dal.ca>
2021-08-11 19:01:55 -0300

    my_strcpy

---
+++ Aug 11th/My_strcpy.c
@@ -0,0 +1,132 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+//des is the destination , pintes to the target memory
+//src is the source , pointes to the target string
+void my_strcpy(char* des, char* src) {
+	if (des == NULL ||  src == NULL) {
+		return;
+	}
+	while (*src != '\0') {
+		*des = *src;
+		des++;
+		src++;
+	}
+	//we also need '\0' as part of the string
+	*des = *src;
+}
+
+//to improve the code
+
+
+void my_strcpy_improved(char* des, char* src) {
+
+	assert(des != NULL);
+	assert(src != NULL);
+	//this will help for better understanding
+
+	while (*src != '\0') {
+		*des++ = *src++;
+	}
+	*des = *src; //for '\0'
+}
+
+void my_strcpy_improved_again(char* des, char* src) {
+	assert(des && src);
+	while (*des++ = *src++) {
+		;
+	}
+	//when *src  = '\0', the ASCII for that is 0 -> false. so the loop will break
+}
+
+
+//to avoid someone put the des and src in the wrong postion
+
+//after adding const
+//Robustness
+
+void my_strcpy_improved_again_second(char* des, const char* src) {
+	assert(des && src);
+	while (*des++ = *src++) {
+		;
+	}
+	//when *src  = '\0', the ASCII for that is 0 -> false. so the loop will break
+}
+
+
+
+int main() {
+
+	//strcpy
+
+	char arr1[] = "abcdef"; // length = 7 there is a '\0' after 'f'
+	char arr2[20] = { 0 };
+
+
+	strcpy(arr2, arr1); //copy arr1 and paste into arr2
+	printf("%s\n", arr2);
+
+	//create a strcpy
+	char arr3[] = "abcdeff";
+	my_strcpy(arr2, arr3);
+	printf("%s\n", arr2);
+
+	char arr4[] = "abcdefff";
+	my_strcpy_improved(arr2, arr4);
+	printf("%s\n", arr2);
+
+
+
+	char arr5[] = "abcdeffff";
+	my_strcpy_improved_again(arr2, arr5);
+	printf("%s\n", arr2);
+
+
+
+	printf("\n");
+
+	const int num = 10;
+	//num = 20; //error
+	int n = 100;
+	int* p = &num;
+	*p = 20;
+	//haha
+	printf("%d \n", num); //num -> 20
+
+	//how we make sure that will not happen?
+
+	//we can use const for pointer
+
+	const int* pp = &num; // equal to int const * pp
+	//*pp = 30;//error
+	//but
+	pp = &n;
+
+	printf("%d \n", num); //num -> 20
+	//Summary
+	//const int* pp -> here const is used for (*pp), and it can not changed by pointers
+	//but pointer itself can be changes, here pp is pointed to num, it can be changed to n
+
+
+
+
+
+
+	int* const ppp = &num;
+	*ppp = 200;//work
+	//ppp = &n; will not work
+
+
+	printf("%d \n", num); //num -> 200
+	//Summary
+	//int* const ppp -> here const is used for ppp(pointer), what (*ppp) is can be changed
+	//but pointer itself can not be changed, here ppp is pointed to num = 10(assume it is 10), it can change the 10 to 100 or 100000. etc
+	//but it can not be other pointers
+
+	//or
+	const int* const pppp = &num;
+
+	return 0;
+}
