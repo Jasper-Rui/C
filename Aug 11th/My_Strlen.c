4c609e77b54f3331d94e09aedbe3a131278f9397
Haorui Yang <hr972438@dal.ca>
2021-08-11 21:02:24 -0300

    my_strcpy

---
+++ Aug 11th/My_Strlen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <assert.h>
+
+
+int my_strlen(char* input) {
+	assert(input != NULL);
+	int count = 0;
+	while (*input != '\0') {
+		count++;
+		input++;
+	}
+	return count;
+}
+
+int my_strlen_improve(const char* input) {
+	assert(input != NULL);
+	int count = 0;
+	while (*input++){
+		count++;
+	}
+	return count;
+}
+
+
+int main() {
+
+	char arr[] = "abcedf";
+	printf("%d \n", my_strlen(arr));
+	printf("%d \n", my_strlen_improve(arr));
+
+
+
+	return 0;
+}
