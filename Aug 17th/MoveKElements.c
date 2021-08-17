44a1f09a7345717fdb45badd3149d6d7561c53d5
Haorui Yang <haoruiyang@Haoruis-MacBook-Air.local>
2021-08-17 14:59:11 -0300

    update

---
+++ Aug 17th/MoveKElements.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+//Question:
+//move k elements to the end of the string
+//example: ABCD -> 2 -> CDAB
+
+
+//1: Get inpunt and store into an array
+//2: Get the k numbers that user want to move
+//3: 
+
+
+
+
+
+int main(){
+
+
+    char arr[10] = {0};
+    int k = 0;
+    int i = 0;
+    while(scanf("%c", &arr[i]) != EOF){
+        i++;
+    }
+
+    printg("%s", arr);
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
