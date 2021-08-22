33db57b870ad9bc580d48cb449869949b582b41d
Jasper Yang <83082501+Jasper-Yang-715@users.noreply.github.com>
2021-08-22 14:02:48 -0300

    pointers to pointers

--- Aug 22nd/Test.c
+++ Aug 22nd/Test.c
@@ -4,6 +4,10 @@ void test(int* a){
 
 }
 
+void test1(int ** p){
+
+}
+
 int main(){
     //what can we pass to test(int*) ?
 
@@ -14,6 +18,16 @@ int main(){
     test(&a);
     test(arr);
     test(NULL);//NOT SUGGESTED
+
+
+    int c = 100;
+    int * d = &c;
+    int ** f = &d;
+
+    test1(f);
+    //or
+    test1(&d);
+
 
 
     return 0;
