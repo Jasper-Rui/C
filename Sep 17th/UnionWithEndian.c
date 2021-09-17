0287980432c586f2e17b66031008890966a5cfc4
Jasper Yang <83082501+Jasper-Yang-715@users.noreply.github.com>
2021-09-17 14:16:25 -0300

    use union to find big or little endian

--- Sep 17th/UnionWithEndian.c
+++ Sep 17th/UnionWithEndian.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
 
+
 int main () {
 
+    union U
+    {
+    /* data */
+    char c;
+    int i;
+    }u;
 
+    u.i = 1;
 
-
-
-
-
-
+    if(u.c == 1){
+       printf("little endian");
+    }
+    else {
+        printf("Big endian");
+    }
 
     return 0;
 }
