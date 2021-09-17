86e1c3be6809ec84905093d3e01364f437d64c44
Jasper Yang <83082501+Jasper-Yang-715@users.noreply.github.com>
2021-09-17 14:01:10 -0300

    update union

--- Sep 17th/UnionExample.c
+++ Sep 17th/UnionExample.c
@@ -8,6 +8,17 @@ union UnionExample
 };
 
 int main () {
+    union UnionExample u = {0};
+    printf("%d\n", sizeof(u));
+    //why this is 4 ?
+
+    printf("%p\n", &u); //0x16fc875d8
+    printf("%p\n", &(u.c1)); //0x16fc875d8
+    printf("%p\n", &(u.i1)); //0x16fc875d8
+    //they have the same address
+    //why
+    
+
 
 
 
