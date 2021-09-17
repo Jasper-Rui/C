23f1ea5ec118519d114a308f404cad0198d02fd5
Jasper Yang <83082501+Jasper-Yang-715@users.noreply.github.com>
2021-09-17 08:53:42 -0300

    bit field

--- Sep 17th/Bit field.c
+++ Sep 17th/Bit field.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 
+struct BitField
+{
+    int _a: 2; //2bits
+    int _b: 6; //6bits
+    int _c: 10; //10bits
+    int _d: 20; //20bits
+};
+
+struct s
+{
+    /* data */
+    char a : 3;
+    char b : 4;
+    char c : 5;
+    char d : 6;
+};
+
+
 int main () {
 
+    printf("%d\n", sizeof(struct BitField));
+    //result is 8
 
-
-
-
+    printf("%d\n", sizeof(struct s));
+    
 
 
     return 0;
