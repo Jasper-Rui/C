0d3a0af72d9d598ab5e88a65ddfca95fc8af1189
Jasper Yang <83082501+Jasper-Yang-715@users.noreply.github.com>
2021-09-19 12:40:57 -0300

    3401

---
+++ Sep 19th/Test.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+int convertToNumber(char* arr){
+    assert(arr);
+    int len = strlen(arr);
+    //calculater the sum
+    int sum1 = 0; 
+    int sum2 = 0;  
+    for(int j = 0; j < len; j++){
+        //88 in ASCII represent character 'X'
+        int a = *arr - '0';
+        if((a != -3) && (*arr != 88)){
+            sum1 += a;
+            sum2 += sum1;
+        }
+        else if((*arr) == 88){
+            sum1 += 10;
+            sum2 += sum1;
+        }
+        *arr++;
+    }
+    *arr--;
+    //88 in ASCII represent character 'X'
+    if((*arr) != 88){
+        if(sum2 % (*arr - '0') == 0) return 1;
+        else return 0;
+    }
+    else{
+        if(sum2 % 11 == 0) return 1;
+        else return 0;
+    }
+}
+
+int main () {
+    char arr[20];
+    //while(scanf("%[^\n]s", &arr) != EOF){
+    while(gets(arr) != EOF){   
+        int result = convertToNumber(arr);
+        if(result == 1){
+            printf("%s is valid\n", arr);
+            memset(arr, 0, sizeof arr);//reset arr[20] to null
+        }
+        else {
+            printf("%s is invalid\n", arr);
+            memset(arr, 0, sizeof arr);//reset arr[20] to null
+        }
+        break;
+    }
+    return 0;
+}
