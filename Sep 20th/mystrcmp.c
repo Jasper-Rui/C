7680d7c391e1bf550f90cf7d2a54a47be0554fe5
Jasper Yang <83082501+Jasper-Yang-715@users.noreply.github.com>
2021-09-20 17:56:30 -0300

    Create mystrcmp.c

---
+++ Sep 20th/mystrcmp.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void set (int * arr, int sz){
+    int i = 0;
+    for(i = 0; i < sz; i++){
+        *(arr + 1) = 0;
+    }
+}
+
+int main () {
+    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+
+
+
+
+    return 0;
+}
