e99e08f509999b999fc2995e96c712776aa2a6b0
Jasper Yang <83082501+Jasper-Yang-715@users.noreply.github.com>
2021-09-21 19:05:09 -0300

    improved memmove

--- Sep 21st/My_memmove.c
+++ Sep 21st/My_memmove.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
+
 
 void * my_memmove (void * des, const void * src, size_t sz){
+    assert(des && src);
+    assert(des >= src + sz);
     void* temp = des;
     
     char arr[sz];
     size_t i = 0;
-    printf("%d\n", sz);
     for(i = 0; i < sz; i++){
         arr[i] = *(char*)src;
         src = (char*)src + 1;
     }
-    printf("%s\n", arr);
+    for(i = 0; i < sz; i++){
+        *(char*)des = arr[i];
+        des = (char*)des + 1;
+    }
     return temp;
 }
 
@@ -19,7 +25,7 @@ void * my_memmove (void * des, const void * src, size_t sz){
 int main () {
     char arr1[10] = "abcdef";
     char arr2[10] = "123";
-    char* ret = my_memmove(arr1, arr2, 3);
-    //printf("%s\n", arr1);
+    char* ret = my_memmove(arr1, arr2, 3 * sizeof(char));
+    printf("%s\n", ret);
     return 0;
 }
