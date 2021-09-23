44d2b8349d519d125d8f4c8b1f71c0422da4090d
Jasper Yang <83082501+Jasper-Yang-715@users.noreply.github.com>
2021-09-23 00:08:50 -0300

    struct size with memory alignment

--- Sep 22rd/StructSize.c
+++ Sep 22rd/StructSize.c
@@ -10,12 +10,34 @@ struct S1 {
 };
 
  
- struct S2 {
+struct S2 {
     int a; // 4
     char c1; // 1 
     char c2; // 1
 };
 
+struct S3 {
+    char c1; // 1 
+    char c2; // 1
+    int a; // 4
+};
+
+
+struct S4 {
+    double d1; // 8 8 8 
+    char c2; // 1 8 1
+    int a; // 4 8 4
+};
+// -> 16
+
+
+struct S5 {
+    double d1; // 8 8 8 
+    struct S4 s;// 16 16 16
+    int a; // 4 8 4
+};
+
+// -> 32
 
 int main () {
 
@@ -23,9 +45,19 @@ int main () {
     printf("%d\n", sizeof(struct S1)); // -> 12 now
     printf("%d\n", sizeof(s)); // -> 12 now
     printf("%d\n", sizeof(struct S2)); // -> 8 now
+    printf("%d\n", sizeof(struct S3)); // -> 8 now
+    printf("%d\n", sizeof(struct S4)); // -> 16 now
+    printf("%d\n", sizeof(struct S5)); // -> 16 now
     //why there is a difference?
     //explaination
     // -> memory alignment
+
+    //rules:
+    //1. 1st member always been put at the 0 index
+    //2. from the 2nd member, it will always been put at n times (a specific number)
+    //a specific number -> default + data type size
+    //3. the overall size for a struct must be n times (max data type size)
+    //
 
     return 0;
 }
