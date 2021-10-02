a92ad6ea6739892500a4360b37e2ba1d08c59cdb
Jasper Yang <83082501+Jasper-Yang-715@users.noreply.github.com>
2021-10-02 16:53:18 -0300

    update

--- Oct 1st/ASN3.c
+++ Oct 1st/ASN3.c
@@ -1,30 +1,34 @@
 // File: ASN3.c Author: Yueming Zou
 // Solution to assignment 3, ECED 3401 Fall 2021
 
-#include <stdio.h>
-#include <string.h>
-#include <assert.h>
-#include <stdlib.h>
 
 //include header file
 #include "ASN3.h"
 
-
 //print mazz function
 void PrintMazz(char * input){
+
     int len = strlen(input);
+
     int times = 0;
 
     for(int i = 0; i < len; i++){
+        // input -> 1st char location/memory
+        // (input + i) -> i char 
+        // *(input + i) i = 1 -> *(T's location) -> 'T'
+
+        //48 - 57
+        //
         if(*(input + i) - 48 >= 0 && *(input + i) - 48 <= 9){
             //from ASCII we know that 48 represent '0' as a char
             //any character from 48 to 57 minus 48 will become a number
             //and it can be represented by a int 
-            times += *(input + i) - 48;
+            times = times + (*(input + i) - 48);
         }
+        // A 65 - Z 90
         else if(*(input + i) >= 65 && *(input + i) <= 90){
             //in ASCII 65 to 90 represent character from 'A' to 'Z'
-            while (times){
+            while (times != 0){
                 printf("%c", *(input + i));
                 times--;
                 //finally times will becomes 0 again for next round
@@ -49,6 +53,7 @@ void PrintMazz(char * input){
             }
         }
     }
+
     printf("\n");
 }
 
@@ -58,12 +63,14 @@ int main () {
     char input[132] = {0};
     //initialize a file pointer
     FILE * fptr = NULL;
-    /* 
+    
+    fptr = fopen("input.txt", "r");
+     /* 
      * Open file in r (read) mode. 
      * "input.txt" is complete path to read file
      */
-    fptr = fopen("input.txt", "r");
     /* fopen() return NULL if last operation was unsuccessful */
+
     if(fptr == NULL)
     {
         /* File not read hence exit */
@@ -71,9 +78,15 @@ int main () {
         exit(EXIT_FAILURE);
     }
 
-    if(fptr) { 
+    //if(NULL -> 0 -> FALSE)
+
+    if(fptr != 0) { 
+        //feof -> file eof -> end of file
+        int result = feof(fptr);
+        //0
          while(!feof(fptr)) { 
-             fgets(input, sizeof(input), fptr); 
+             fgets(input, sizeof(input), fptr);
+
              //check index of each char
              //if it's a number from 0 - 9 check ascii
              //if it's a char from A to Z
@@ -81,6 +94,7 @@ int main () {
              //! means \n
              //b means a space
              //* for
+
              PrintMazz(input);
              //printf("%s", input);
          } 
--- Oct 1st/ASN3.h
+++ Oct 1st/ASN3.h
@@ -1,16 +1,14 @@
 #ifndef __ASN3_HEADER
 #define __ASN3_HEADER
+
 #include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
 
 
 void PrintMazz(char * input);
-
-
-
-
-
-
-
 
 
 #endif
